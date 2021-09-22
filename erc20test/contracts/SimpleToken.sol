cbfcde61f28615fb15a31959629549b48eb23c0c
jordanteague <ejteague@gmail.com>
2021-09-21 23:53:46 -0400

    Testing compilation of contracts that import OpenZeppelin ERC20 contract, as well as compiling/deploying in Truffle

---
+++ erc20test/contracts/SimpleToken.sol
@@ -0,0 +1,28 @@
+// contracts/SimpleToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @title SimpleToken
+ * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
+ * Note they can later distribute these tokens as they wish using `transfer` and other
+ * `ERC20` functions.
+ * Based on https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.1/contracts/examples/SimpleToken.sol
+ */
+contract SimpleToken is ERC20 {
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+
+    address public manager;
+    
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply
+    ) ERC20(name, symbol) {
+        _mint(msg.sender, initialSupply);
+    }
+}
