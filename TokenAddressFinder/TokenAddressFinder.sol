c9a844a2c282390a732cd44004374c729f9a4a30
jordanteague <ejteague@gmail.com>
2021-10-04 22:18:39 -0400

    testing simple version of token address finder contract

---
+++ TokenAddressFinder/TokenAddressFinder.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.9;
+
+contract TokenAddressFinder {
+
+    mapping(string => address) public tokens;
+
+    address public admin; //@dev LexDAO Gnosis vault?
+
+    modifier onlyAdmin {
+        require(msg.sender == admin);
+        _;
+    }
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    //@dev to keep things simple, no case control. just enter all lowercase. (or case control in a UX)
+    function addToken(string memory symbol_, address tokenAddress_) public onlyAdmin {
+       tokens[symbol_]  = tokenAddress_; //@dev immutable, no way to change address or delete token once added
+    }
+
+    function getTokenAddress(string memory symbol_) external view returns(address _tokenAddress) {
+        require(tokens[symbol_] != address(0), "This token does not exist, try entering in all lowercase");
+        _tokenAddress = tokens[symbol_];
+        return _tokenAddress;
+    }
+
+}
