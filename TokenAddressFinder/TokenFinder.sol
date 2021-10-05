38d1db9fa1b60836722519e894460d07ebeb1900
jordanteague <ejteague@gmail.com>
2021-10-05 07:59:48 -0400

    removed my incorrect code comment re: immutability

--- TokenAddressFinder/TokenFinder.sol
+++ TokenAddressFinder/TokenFinder.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.9;
 
 contract TokenFinder {
     address public lexDAO;
-    
+
     mapping(string => address) public tokens;
 
     modifier onlyLexDAO {
@@ -18,14 +18,14 @@ contract TokenFinder {
 
     /// @dev To keep things simple, no case control - just enter all lowercase (or case control in a UX).
     function addToken(string calldata symbol_, address tokenAddress_) external onlyLexDAO {
-        tokens[symbol_] = tokenAddress_; /// @dev Immutable, no way to change address or delete token once added.
+        tokens[symbol_] = tokenAddress_;
     }
 
     function getToken(string calldata symbol_) external view returns (address tokenAddress) {
-        require(tokens[symbol_] != address(0), "This token does not exist, try entering in all lowercase");
+        require(tokens[symbol_] != address(0), "This token does not exist, try entering in all uppercase or all lowercase");
         tokenAddress = tokens[symbol_];
     }
-    
+
     /// @notice Protocol for LexDAO to update role.
     /// @param _lexDAO Account to assign role to.
     function updateLexDAO(address _lexDAO) external onlyLexDAO {
