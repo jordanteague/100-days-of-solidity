9680bacd154584d866aad5218af5b5b844ccbf26
jordanteague <ejteague@gmail.com>
2021-10-10 22:32:46 -0400

    removed a comment

--- LexWhiteListableMerkle/test/whitelist-test.js
+++ LexWhiteListableMerkle/test/whitelist-test.js
@@ -16,8 +16,6 @@ describe("LexNFT", function () {
 
       await lexNFT.deployed();
 
-      //list = await lexNFT.getList();
-
       list = [
           '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
           '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
